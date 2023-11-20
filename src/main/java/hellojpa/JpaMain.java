8bb5f868bac31a4c4e9a6e05c17e3f144047fa06
정소민 <yjk70702002@gmail.com>
2023-11-20 20:35:46 +0900

    객체를 테이블에 맞추어 모델링

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,10 +17,21 @@ public class JpaMain {
         //엔티티 매니저는 데이터 변경시 트랜잭션을 시작해야 한다.
 
         try {
-            Member member = new Member();
-            member.setUsername("A");
+            Team team = new Team();
+            team.setName("TeamA");
+            entityManager.persist(team);
 
+            Member member = new Member();
+            member.setUsername("Member1");
+            member.setTeamId(team.getId());
             entityManager.persist(member);
+
+            //조회
+            Member findMember = entityManager.find(Member.class, member.getId());
+            //teamId 가져오기
+            Long findTeamId = findMember.getTeamId();
+            //연관관계가 없음
+            Team findTeam = entityManager.find(Team.class, findTeamId);
 
             entityTransaction.commit(); // [트랜잭션] 커밋
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -9,10 +9,13 @@ import javax.persistence.*;
         initialValue = 1, allocationSize = 1)
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
-    @Column(name = "name", nullable = false)
+    @Column(name = "USERNAME")
     private String username;
+    @Column(name = "TEAM_ID")
+    private Long teamId;
 
     public Member() {
 
@@ -32,5 +35,13 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
     }
 }
