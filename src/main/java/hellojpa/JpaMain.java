cc881712551c316a4ae8b3fc9078e3651e11731f
정소민 <yjk70702002@gmail.com>
2023-11-21 17:02:54 +0900

    객체 지향 모델링

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -21,17 +21,21 @@ public class JpaMain {
             team.setName("TeamA");
             entityManager.persist(team);
 
+            Team team2 = new Team();
+            team2.setName("TeamB");
+            entityManager.persist(team2);
+
             Member member = new Member();
             member.setUsername("Member1");
-            member.setTeamId(team.getId());
             entityManager.persist(member);
 
             //조회
-            Member findMember = entityManager.find(Member.class, member.getId());
-            //teamId 가져오기
-            Long findTeamId = findMember.getTeamId();
-            //연관관계가 없음
-            Team findTeam = entityManager.find(Team.class, findTeamId);
+            Member findMember = entityManager.find(Member.class, member.getId()); // jpa가 member와 team을 join해 한번에 다 가져옴
+
+            Team findTeam = findMember.getTeam();
+
+            Team newTeam = entityManager.find(Team.class, 2L);
+            findMember.setTeam(newTeam);
 
             entityTransaction.commit(); // [트랜잭션] 커밋
         } catch (Exception e) {
